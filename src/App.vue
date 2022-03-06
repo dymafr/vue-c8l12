d7987aed58180f3f71f022e4c20936c44dad4d53
dymafr <erwan1309@hotmail.com>
2022-03-06 13:28:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import TheHeader from './components/Header.vue';
+import TheFooter from './components/Footer.vue';
+import Shop from './components/Shop/Shop.vue';
+import Cart from './components/Cart/Cart.vue';
+import data from './data/product';
+
+import { reactive } from 'vue';
+import type { ProductInterface } from './interfaces';
+import type { ProductCartInterface } from './interfaces';
+
+const state = reactive<{
+  products: ProductInterface[];
+  cart: ProductCartInterface[];
+}>({
+  products: data,
+  cart: [],
+});
+
+function addProductToCart(productId: number): void {
+  const product = state.products.find((product) => product.id === productId);
+  if (product) {
+    const productInCart = state.cart.find(
+      (product) => product.id === productId
+    );
+    if (productInCart) {
+      productInCart.quantity++;
+    } else {
+      state.cart.push({ ...product, quantity: 1 });
+    }
+  }
+}
+function removeProductFromCart(productId: number): void {
+  const productFromCart = state.cart.find(
+    (product) => product.id === productId
+  );
+  if (productFromCart?.quantity === 1) {
+    state.cart = state.cart.filter((product) => product.id !== productId);
+  } else {
+    productFromCart.quantity--;
+  }
+}
+</script>
+
+<template>
+  <div class="app-container">
+    <TheHeader class="header" />
+    <Shop
+      :products="state.products"
+      @add-product-to-cart="addProductToCart"
+      class="shop"
+    />
+    <Cart
+      :cart="state.cart"
+      class="cart"
+      @remove-product-from-cart="removeProductFromCart"
+    />
+    <TheFooter class="footer" />
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/base.scss';
+@import './assets/debug.scss';
+
+.app-container {
+  min-height: 100vh;
+  display: grid;
+  grid-template-areas: 'header header' 'shop cart' 'footer footer';
+  grid-template-columns: 75% 25%;
+  grid-template-rows: 48px auto 48px;
+}
+.header {
+  grid-area: header;
+}
+.shop {
+  grid-area: shop;
+}
+.cart {
+  grid-area: cart;
+  border-left: var(--border);
+  background-color: white;
+}
+.footer {
+  grid-area: footer;
+}
+</style>
---
+++ src/components/Cart/Cart.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CartProductList from './CartProductList.vue';
+import type { ProductCartInterface } from '@/interfaces';
+
+const props = defineProps<{
+  cart: ProductCartInterface[];
+}>();
+
+const emit = defineEmits<{
+  (e: 'removeProductFromCart', productId: number): void;
+}>();
+</script>
+
+<template>
+  <div class="p-20">
+    <h2 class="mb-10">Panier</h2>
+    <CartProductList
+      :cart="cart"
+      @remove-product-from-cart="emit('removeProductFromCart', $event)"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
